31f52438163b0a24dedb269ea9937904b6a9bd41
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2014-04-28 05:55:19 +0000

    Preliminary refactor of encode route to use encoder.

--- routes/encode.js
+++ routes/encode.js
@@ -5,31 +5,18 @@ var
   dirExp = require("node-dir"),
   util = require('util'),
   path = require("path"),
-  fs = require("fs");
+  fs = require("fs"),
+  Encoder = require(__dirname + '/../lib/encoder').Encoder;
 
 module.exports = function(app, upload) {
   var // App level variables
-    // encodeDir = app.get("encodeDir") || "./build/encode",
-    // videoDir = app.get("movieDir") || "./build/videos",
-    // uploadDir = app.get("uploadDir") || "./build/encode",
     encodeDir = "./build/encode",
     videoDir = "./build/videos",
-    uploadDir = "./build/encode",
-
-    // Module variables
-    fileEncodeOptions = {
-      encoder: "x264",
-      "keep-display-aspect":true,
-      modulus:16,
-      vb:"2500",
-      quality:"20",
-      "crop":"0:0:0:0"},
-    movieExt = ".mp4",
-    encodeQueue = {},
+    videoDir = "./build/upload",
+    encoder = new Encoder();
     PATHSEP = path.sep;
 
   var serverConfig = function(app, upload) {
-    uploadDir = "./build/uploads";
     upload.on("end", uploadFinished);
     setupRoutes(app);
   }
@@ -66,64 +53,32 @@ module.exports = function(app, upload) {
     });  
   }
 
-  var uploadFinished = function(fileInfo) {
-    var 
-      vid = PendingVideo(fileInfo);
+    var uploadFinished = function(fileInfo) {
+        var inputName = uploadDir + '/' + fileInfo.name
+        encoder.encode(inputName, './build/encode')
+            .on('error', function (err) {
+                console.log('Error while encoding: ', err);
+            })
+            .on('progress', function (vid) {
 
-    fileEncodeOptions.input = vid.tmpName;
-    fileEncodeOptions.output = vid.tmpName;
+            })
+            .on('complete', function (vid) {
+                var endName = videoDir + "/" + fileInfo.name + movieExt;
+                fs.rename(vid.output, endName, function(err) {
+                    if (err) {
+                        console.log('Error renameing ', err);
+                        return;
+                    }
 
-    encodeUploadedMovie(vid, fileEncodeOptions);
-  }
-
-  var encodeUploadedMovie = function(fileInfo, encOptions) {
-    //DEBUG
-    console.log("Adding file to encode QUEUE: %s", fileInfo.name);
-    console.log("encOptions", encOptions);
-    console.log("fileInfo", fileInfo);
-
-    var handle = handbrake.spawn(encOptions)
-    .on("complete", function(params) {
-      console.log("FINISH encoding for: \n\t%s", fileInfo.name);
-      encodeComplete(fileInfo)
-    });
-
-    setupEndpoint(handle, fileInfo.uniqueKey);
-  }
-
-  var encodeComplete = function(fileInfo) {
-    var endName = videoDir + "/" + fileInfo.name + movieExt;
-    
-    console.log("Moving encoded file to dir: " + endName);
-    // move to video dir
-    fs.rename(fileInfo.tmpName, endName, function(err) {
-      if(err)
-        console.log(err);
-    })
-
-    //remove from encode  queue
-    delete encodeQueue[fileInfo.uniqueKey];
-    
-    //remove from upload dir
-    fs.unlink(fileInfo.uploadDirFile,function(err){
-      if(err)
-        console.log(err);
-    });
-  }
-
-  var setupEndpoint = function(handle, fileId) {
-    encodeQueue[fileId] = {};
-
-    handle.on("error", function(err){
-        console.log(err.message);
-        delete encodeQueue[fileId];
-    })
-    .on("progress", function(progress){
-      console.log("P2 - progress " + progress.percentComplete + " ETA: " + progress.eta);
-      encodeQueue[fileId].eta = progress.eta;
-      encodeQueue[fileId].complete = progress.percentComplete;
-    });
-  }
+                    fs.unlink(vid.input, function(err){
+                        if (err) {
+                            console.log('Error unlinking ', vid.input, err);
+                            return;
+                        }
+                    });
+                });
+            });
+    };
 
   /*
     Get a list of the videos currently being encoded
@@ -155,46 +110,12 @@ module.exports = function(app, upload) {
     });
   }
 
-  /** Pending video
-
-    filename - filename (includes extension)
-    name - filename with no extension
-    uniqueKey - key to set the tmp filename to
-    extension - file extension ie. mp4, avi, etc...
-    tmpName - filename using unique key
-  */
-  function PendingVideo(fileInfo) {
-    if(!(this instanceof PendingVideo))
-      return new PendingVideo(fileInfo);
-    
-    this.filename = fileInfo.name;
-    this.uniqueKey = "a" + Date.now();
-  };
-
-  PendingVideo.prototype = {
-    get tmpName(){
-      return encodeDir + '/' + this.uniqueKey + movieExt
-    },
-    get extension() {
-      return path.extname(this.filename);
-    },
-    get name() {
-      return path.basename(this.filename,this.extension);
-    },
-    get uploadDirFile() {
-      if(typeof uploadDir === 'undefined')
-        uploadDir == '';
-      return uploadDir + '/' + this.filename;
-    }
-  };
-
   if(arguments.length == 2) {
     serverConfig(app, upload);
     return {"getProcessing": getProcessing};
   } else {
     // cli init
     console.log('console init');
-    uploadDir = "./build/encode";
     var encodeVid = function(file, out) {
       fileEncodeOptions.input = file;
       fileEncodeOptions.output = out;
